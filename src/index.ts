6121900cba83238a093fa716fa9c8736f2ff4b57
kaibocai <89094811+kaibocai@users.noreply.github.com>
2023-12-13 08:17:40 -0600

    minor updates on build, tests (#24)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello World");
+import { TaskHubGrpcClient } from "./client";
+import { TaskHubGrpcWorker } from "./worker/task-hub-grpc-worker";
+import { OrchestrationContext } from "./task/context/orchestration-context";
+import { ActivityContext } from "./task/context/activity-context";
+
+export { TaskHubGrpcClient, TaskHubGrpcWorker, OrchestrationContext, ActivityContext };
