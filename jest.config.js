e5ad65a79c719a99efede86a3bfe7ec1b1e7ec36
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2026-01-28 11:13:43 -0800

    module mapping

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.spec.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^@microsoft/durabletask-js$": "<rootDir>/packages/durabletask-js/src/index.ts",
+    "^@microsoft/durabletask-js-azuremanaged$": "<rootDir>/packages/durabletask-js-azuremanaged/src/index.ts",
+  },
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
